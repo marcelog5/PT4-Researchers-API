f29d9e0d5b04cf54ad95a8a79c5b3a545fd78f4b
marcelog5 <marcelovictorg2@gmail.com>
2020-10-10 10:51:41 -0300

    Install Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello world' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello world' }));
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+  console.log('Server started on port 3333!');
 });
