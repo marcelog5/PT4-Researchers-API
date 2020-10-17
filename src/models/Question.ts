7fa0bda9d1bb671b9083c8c591017eaf81113bca
marcelog5 <marcelovictorg2@gmail.com>
2020-10-17 10:31:02 -0300

    Creat model to table Question

---
+++ src/models/Question.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Question')
+class Question {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  question: string;
+
+  @Column('boolean')
+  inverted: boolean;
+
+  @Column('varchar')
+  trait: string;
+
+  @Column('varchar')
+  factor: string;
+}
+
+export default Question;
