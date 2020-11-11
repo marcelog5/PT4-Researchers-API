1d0f8395bdc7d0cb965c67baf1bb3e119dbdff33
marcelog5 <marcelovictorg2@gmail.com>
2020-11-10 22:28:53 -0300

    Create migrations to User

---
+++ src/database/migrations/1605006504008-CreateUsers.ts
@@ -0,0 +1,77 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1605006504008 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'gender',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'age',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'Institution',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'ORCID',
+            type: 'varchar',
+            isNullable: true,
+            isUnique: true,
+          },
+          {
+            name: 'Lattes',
+            type: 'varchar',
+            isNullable: true,
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('respondents');
+  }
+}
