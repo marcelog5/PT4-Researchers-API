01f2e940fac0696c01599f55c60eae562b546089
marcelog5 <marcelovictorg2@gmail.com>
2021-02-08 14:39:35 -0300

    Added Temporary config to stmp

---
+++ src/config/mail.ts
@@ -0,0 +1,11 @@
+class Configs {
+  public host = 'smtp.gmail.com';
+
+  public port = 587;
+
+  public user = 'pt4.researchers@gmail.com';
+
+  public password = '5vKTl7grbi%D';
+}
+
+export default new Configs();
